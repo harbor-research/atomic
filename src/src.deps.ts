62c81f6a457c1cdb74b606aaaa11bdfd0220eba2
mcgear <michael.gearhardt@gmail.com>
2023-10-04 10:38:42 -0600

    upgrades

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,2 +1,2 @@
 export * from "https://deno.land/x/fathym_atomic@v0.0.86-integration/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_chat@v0.0.7-integration/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_chat@v0.0.8-integration/mod.ts";
