9fc84e92a364cffb7eb99388194420f868c729bf
mcgear <michael.gearhardt@gmail.com>
2023-10-05 10:36:36 -0600

    Upgrades and ChatInput export passthrough

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,2 +1,4 @@
-export * from "https://deno.land/x/fathym_atomic@v0.0.86-integration/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_chat@v0.0.14-integration/mod.ts";
+// export * from "../../../fathym-deno/atomic/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.87-integration/mod.ts";
+// export * from "../../../fathym-deno/atomic-chat/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_chat@v0.0.15-integration/mod.ts";
