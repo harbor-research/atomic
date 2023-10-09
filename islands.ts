91368ab6100c1b7424769d8e4a8a52473a1e7cbf
mcgear <michael.gearhardt@gmail.com>
2023-10-09 13:51:44 -0600

    Chart export

--- islands.ts
+++ islands.ts
@@ -1,2 +1,2 @@
 export { ChatInput } from "./src/molecules/_exports.tsx";
-export { Chart } from "https://deno.land/x/fathym_atomic_reports@v0.0.2-integration";
+export { Chart } from "./src/src.deps.ts";
