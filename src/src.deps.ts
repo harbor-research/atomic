c36627e63108347835f4795e772255229704f826
mcgear <michael.gearhardt@gmail.com>
2023-10-11 18:48:06 -0600

    Upgrades

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -2,7 +2,7 @@
 // export * from "../../../fathym-deno/atomic-chat/mod.ts";
 // export * from "../../../fathym-deno/atomic-reports/mod.ts";
 // export * from "../../../fathym-deno/atomic-reports/islands.ts";
-export * from "https://deno.land/x/fathym_atomic@v0.0.92/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.93-integration/mod.ts";
 export * from "https://deno.land/x/fathym_atomic_chat@v0.0.18/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_reports@v0.0.10/mod.ts";
-export * from "https://deno.land/x/fathym_atomic_reports@v0.0.10/islands.ts";
+export * from "https://deno.land/x/fathym_atomic_reports@v0.0.11-integration/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_reports@v0.0.11-integration/islands.ts";
